73dda966087cd7d517c8fdf758814933026d14da
Bingzhi Zhao <bz2746@nyu.edu>
2025-06-22 22:16:01 -0400

    Update script.R

--- script.R
+++ script.R
@@ -1,2 +1,17 @@
 library(tidyverse)
 fisc <- read_csv("FiSC-Full-Dataset-2022-Update.csv")
+
+fisc_small <- fisc %>%
+  select(city_name, year, rev_general_city, spending_general_city)
+
+fisc_filtered <- fisc_small %>%
+  filter(year >= 2015)
+
+fisc_mutated <- fisc %>%
+  mutate(annual_balance = rev_general_city - spending_general_city)
+
+fisc_summary <- fisc_mutated %>%
+  group_by(city_name) %>%
+  summarize(avg_annual_balance = mean(annual_balance, na.rm = TRUE))
+
+write_csv(fisc_summary, "annual_balance_summary.csv")
